c35317f3777c8af09b3ff2e7ecf57195dc6ac4c5
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2022-04-10 18:24:50 -0400

    Minor validation fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
 
   has_one_attached :avatar, :dependent => :destroy
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: 'Must be a valid email address' }
-  validates :avatar, presence: :true
 end
