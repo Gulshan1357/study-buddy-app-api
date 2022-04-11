c6b3ff8a57dc22343090f06e208b3433d8f262eb
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2022-04-10 20:21:13 -0400

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+User.reset_pk_sequence
+Slot.destroy_all
+Slot.reset_pk_sequence
+Meeting.destroy_all
+Message.destroy_all
 
-if User.count < 10
-  10.times do
-    u = User.create(
-      email: FFaker::Internet.email,
-      password: 'random',
-      password_confirmation: 'random',
-      first_name: FFaker::Name.first_name,
-      last_name: FFaker::Name.last_name,
-      details: FFaker::Lorem.phrase,
-    )
-    u.avatar.attach(io: File.open(File.join(Rails.root,'app/assets/images/avatar.png')), filename: 'avatar.png')
-  end
-end
+uc = User.create!(
+  email: 'chris@go.com',
+  password: 'aaa',
+  password_confirmation: 'abc',
+  first_name: 'Chris',
+  last_name: 'Hemsworth',
+  details: 'I am a fourth year student. I also love acting, reading books and nature. ',
+  )
+uc.avatar.attach(io: File.open(File.join(Rails.root,'app/assets/images/chris.jpg')), filename: 'chris.jpg')
+
+User.first.Slot.create(start_time: '2022-04-10 19:00:00')
+
+ue = User.create!(
+  email: 'elsa@go.com',
+  password: 'sss',
+  password_confirmation: 'abc',
+  first_name: 'Elsa',
+  last_name: 'Pataky',
+  details: 'I am a third year student. I love reading, skiing and chess',
+  )
+ue.avatar.attach(io: File.open(File.join(Rails.root,'app/assets/images/elsa.jpg')), filename: 'elsa.jpg')
+
+User.last.Slot.create(
+  start_time: '2022-04-10 19:00:00',
+  user_id: User.last,
+  )
+
+
+# if User.count < 10
+#   10.times do
+#     u = User.create(
+#       email: FFaker::Internet.email,
+#       password: 'random',
+#       password_confirmation: 'random',
+#       first_name: FFaker::Name.first_name,
+#       last_name: FFaker::Name.last_name,
+#       details: FFaker::Lorem.phrase,
+#     )
+#     u.avatar.attach(io: File.open(File.join(Rails.root,'app/assets/images/avatar.png')), filename: 'avatar.png')
+#   end
+# end
 
